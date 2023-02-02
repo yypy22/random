2b8086382bda521c59e55c1b2c6224068a3a1a07
Pon5522 <99264752+yypy22@users.noreply.github.com>
2023-02-02 10:12:06 +0000

    bit operations

---
+++ bit-1.py
@@ -0,0 +1,14 @@
+#bit 
+
+a = 0b10101001
+b = 0b10001100
+
+print("and",bin(a&b))
+print("or",bin(a|b))
+print("xor",bin(a^b))
+print("~0",~0)
+print("~1",~1)
+print("~-1",~-1)
+print("~-2",~-2)
+print(bin(a<<1))
+print(bin(a>>2))
